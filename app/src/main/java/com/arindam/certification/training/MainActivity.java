221bedb6689619945d721e23a655241dfdec600b
Arindam Karmakar <arindamxd@gmail.com>
2019-06-20 13:20:40 +0530

    initial commit

---
+++ app/src/main/java/com/arindam/certification/training/MainActivity.java
@@ -0,0 +1,14 @@
+package com.arindam.certification.training;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
