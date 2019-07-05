104d3573ca81891141fe696a216028e43e941731
Arindam Karmakar <arindamxd@gmail.com>
2019-07-05 11:49:40 +0530

    app link implemented

---
+++ app/src/main/java/com/arindam/certification/training/payment/PaymentActivity.kt
@@ -0,0 +1,20 @@
+package com.arindam.certification.training.payment
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * Created by Arindam Karmakar on 5/7/19.
+ */
+
+class PaymentActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // ATTENTION: This was auto-generated to handle app links.
+        val appLinkIntent = intent
+        val appLinkAction = appLinkIntent.action
+        val appLinkData = appLinkIntent.data
+    }
+}
